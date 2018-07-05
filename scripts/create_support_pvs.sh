a1bbee9290cdc2a1e5851931c4681322393076dd
Jason Smith <jasosmit@redhat.com>
2018-07-05 10:31:11 -0500

    Corrected the path to the script - added sanity check on when to run

--- scripts/create_support_pvs.sh
+++ scripts/create_support_pvs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p /srv/nfs/user-vols/pv{1..200}
+mkdir -p /srv/nfs/user-vols/pv{0..30}
 
 for pvnum in {0..30} ; do
 echo "/srv/nfs/user-vols/pv${pvnum} *(rw,root_squash)" >> /etc/exports.d/openshift-uservols.exports
